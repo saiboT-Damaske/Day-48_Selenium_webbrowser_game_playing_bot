fa182ca1b108b33ffd2303a836c83392ef1066ec
tobif <saibot01141@gmail.com>
2023-08-07 01:25:38 +0200

    finished bot player

--- cookie_clicker_game.py
+++ cookie_clicker_game.py
@@ -9,15 +9,56 @@ import time
 options = Options()
 options.add_experimental_option("detach", True)
 driver = selenium.webdriver.Chrome(options=options)
-driver.maximize_window()
+# driver.maximize_window()
 
 driver.get("http://orteil.dashnet.org/experiments/cookie/")
 cookie = driver.find_element(By.CSS_SELECTOR, "#cookie")
 buyable_items_prices = driver.find_elements(By.CSS_SELECTOR, "div b")
-for item in buyable_items_prices:
-    print(item.text)
 
-# while True:
-#
-#     cookie.click()
-#     money = driver.find_element(By.CSS_SELECTOR, "#money").text
+items = driver.find_elements(By.CSS_SELECTOR, "#store div")
+item_ids = [item.get_attribute("id") for item in items]
+
+item_prices = []
+prices = driver.find_elements(By.CSS_SELECTOR, "#store b")
+for price in prices:
+    if price.text != "":
+        item_prices.append(int(price.text.strip().split(" - ")[1].replace(",", "")))
+
+# Create dictionary of store items and prices
+cookie_upgrades = {}
+for n in range(len(item_prices)):
+    cookie_upgrades[item_prices[n]] = item_ids[n]
+
+print(cookie_upgrades)
+
+
+timeout = time.time() + 5
+five_min = time.time() + 60*5 # 5minutes
+
+while True:
+
+    cookie.click()
+    money = int(driver.find_element(By.CSS_SELECTOR, "#money").text.replace(",", ""))
+
+
+
+    if time.time() > timeout:
+        affordable = {}
+        for pr, item_id in cookie_upgrades.items():
+            if money >= pr:
+                affordable[pr] = item_id
+
+        highest_price_affordable = max(affordable)
+        print(highest_price_affordable)
+
+        driver.find_element(By.ID, cookie_upgrades[highest_price_affordable]).click()
+
+        timeout = time.time() + 5
+
+    if time.time() > five_min:
+        cookie_per_s = driver.find_element(By.ID, "cps")
+        print(cookie_per_s)
+        break
+
+
+
